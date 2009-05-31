4c11a8b75f2fa42d608f49842e85154d5df5c935
kevin nolan <kevin@stern-server.hsd1.or.comcast.net>
2009-05-31 11:31:34 -0700

    added pos_buffer to timeseries

--- lib/timeseries.rb
+++ lib/timeseries.rb
@@ -18,7 +18,7 @@ class Timeseries
   attr_accessor :timevec, :time_map, :local_range, :price
 
   def initialize(symbol_or_id, local_range, time_resolution, options={})
-    options.reverse_merge! :price => :default, :plot_results => true, :pre_buffer => true, :populate => true
+    options.reverse_merge! :price => :default, :plot_results => true, :pre_buffer => true, :populate => true, :post_buffer => false
     initialize_state()
     self.ticker_id = (symbol_or_id.is_a? Fixnum) ? Ticker.find(symbol_or_id).id : Ticker.lookup(symbol_or_id).id
     raise ArgumentError, "Excpecting ticker symbol or ticker id as first argument. Neither could be found" if ticker_id.nil?
@@ -37,9 +37,10 @@ class Timeseries
       self.start_time = (local_range.begin - day_offset.days).to_time
       self.end_time = (local_range.end + day_offset.days).to_time > Time.now ? Time.now : (local_range.begin + day_offset.days).to_time
     elsif source_model == DailyBar
-      offset = options[:pre_buffer] ? 120 : 0
-      self.start_time = (local_range.begin - offset.days).to_time
-      self.end_time = (local_range.end + offset.days).to_time > Time.now ? Time.now : (local_range.end + offset.days).to_time
+      pre_offset = options[:pre_buffer] ? 120 : 0
+      post_offset = options[:post_buffer] ? 120 : 0
+      self.start_time = (local_range.begin - pre_offset.days).to_time
+      self.end_time = (local_range.end + post_offset.days).to_time > Time.now ? Time.now : (local_range.end + post_offset.days).to_time
     else
       raise ArgumentError, "Bar resolution cannot be retrieved"
     end

c620e3821605f295a122211ecced609963b665f3
Kevin Nolan <kevin@insgraph.com>
2009-11-26 18:24:05 -0800

    fixed the display of error messages

--- app/models/sim_job.rb
+++ app/models/sim_job.rb
@@ -1,13 +1,22 @@
 class SimJob < ActiveRecord::Base
+
   validates_presence_of :user, :position_table, :initial_balance, :order_amount, :minimum_balance, :order_charge
-  validates_presence_of :entry_slippage, :exit_slippage, :log_level, :keep_tables
+  validates_presence_of :entry_slippage, :exit_slippage, :log_level
   validates_numericality_of  :initial_balance, :order_amount, :minimum_balance, :order_charge, :log_level
   validate :portfolio_size_or_reinvest_percent
+  validate :validate_output_directory
 
   def portfolio_size_or_reinvest_percent
      unless portfolio_size.blank? ^ reinvest_percent.blank?
-       errors.add(:portfolio_size, "Portfolio Size and Reinvest Percent cannot both be given")
-       errors.add(:reinvest_percent, "Portfolio Size and Reinvest Percent cannot both be given")
+       errors.add(:portfolio_size, "Reinvest Percent is also given; one one or the other is permitted")
+       errors.add(:reinvest_percent, "See above")
+    end
+  end
+
+  def validate_output_directory
+    return if dir.blank?
+    unless File.exist?(dir) and (fs = File::Stat.new(dir)) and fs.directory? and fs.writable?
+      errors.add(:dir, "output directory: '#{dir}' doesn't exist or isn't a directory or isn't writable")
     end
   end
 
